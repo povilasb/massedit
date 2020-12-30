31c7a1ac07ba00c4014f1fdcf2f3c8d662299a31
Povilas Balciunas <balciunas90@gmail.com>
2020-12-30 14:15:45 +1300

    Use given max_depth always

--- massedit.py
+++ massedit.py
@@ -491,7 +491,7 @@ def get_paths(patterns, start_dirs=None, max_depth=1):
         if directory:
             patterns = [os.path.basename(pattern)]
             start_dirs = directory
-            max_depth = 1
+            # max_depth = 1
 
     start_dirs = start_dirs or "."
     for start_dir in start_dirs.split(","):
